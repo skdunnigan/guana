717fea4426629ebc29ac6b5a47aed28526e87bc4
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-05-23 16:52:25 -0400

    added code

--- R/01_Load_Wrangle_Run.R
+++ R/01_Load_Wrangle_Run.R
@@ -8,7 +8,21 @@ dat <- read_xlsx("data/data.xlsx", sheet = 'Sheet1')
 head(dat)
 str(dat)
 
-# set 'Result' column to numerical value
-## this column contains all the numerical values from the analyses
-dat$Result <- as.numeric(dat$Result)
+# ---------------------------------------------------
+# clean up the data to make writing easier later
+# ---------------------------------------------------
+# organize and remove spaces from column names
+names(dat) <- tolower(names(dat))
+names(dat)<-make.names(names(dat),unique = TRUE) # this will remove spaces in column names and add a .
+
+# ---------------------------------------------------
+# data type housekeeping
+# ---------------------------------------------------
+# set 'Result' column to numerical value, this column contains all the numerical values from the analyses
+dat$result <- as.numeric(dat$result)
+
+# convert DateReceived and DateAnalyzed into POSIXct format
+dat$datereceived <- as.POSIXct(dat$datereceived, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
+dat$dateanalyzed <- as.POSIXct(dat$dateanalyzed, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
+
 
