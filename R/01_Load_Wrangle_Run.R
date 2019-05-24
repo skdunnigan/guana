71a843ed0c1c19de082901909d5658cb606a31af
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-05-24 12:07:54 -0400

    cleaned up and added janitor::clean_names()

--- R/01_Load_Wrangle_Run.R
+++ R/01_Load_Wrangle_Run.R
@@ -2,18 +2,12 @@
 source('R/00_loadpackages.R')
 
 # read in data file
-dat <- read_xlsx("data/data.xlsx", sheet = 'Sheet1')
+dat <- read_xlsx("data/data.xlsx", sheet = 'Sheet1') %>% janitor::clean_names()
+#janitor and clean_names() function cleans up the column header names!
 
 # inspect the data file
 head(dat)
 str(dat)
-
-# ---------------------------------------------------
-# clean up the data to make writing easier later
-# ---------------------------------------------------
-# organize and remove spaces from column names
-names(dat) <- tolower(names(dat))
-names(dat)<-make.names(names(dat),unique = TRUE) # this will remove spaces in column names and add a .
 
 # ---------------------------------------------------
 # data type housekeeping
@@ -22,7 +16,7 @@ names(dat)<-make.names(names(dat),unique = TRUE) # this will remove spaces in co
 dat$result <- as.numeric(dat$result)
 
 # convert DateReceived and DateAnalyzed into POSIXct format
-dat$datereceived <- as.POSIXct(dat$datereceived, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
-dat$dateanalyzed <- as.POSIXct(dat$dateanalyzed, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
+dat$date_received <- as.POSIXct(dat$date_received, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
+dat$date_analyzed <- as.POSIXct(dat$date_analyzed, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
 
 
