f0f9c6c3854ee2296f8498d1d923b05f31e38379
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-17 19:45:21 +0000

    updated figures for chlorophyll

--- R/02_chlorophyll.R
+++ R/02_chlorophyll.R
@@ -56,9 +56,10 @@ dat4 %>%
         panel.grid.minor = element_blank(),
         panel.grid.major = element_line(color='gray95'))+
   scale_y_continuous(expand = c(0,0))+
-  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b')+
+  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y')+
   labs(x = '', y = chla_y_title,
-       title = "Lake Sites")
+       title = "Lake Sites",
+       caption = "Sites near water control structures are sampled for hydrologic connectivity and not used for waterbody assessments")
 
 # all river sites with regulation sites and excluded sites shown
 dat4 %>%
@@ -92,19 +93,52 @@ dat4 %>%
   filter(WBID == "River" & component_short == "CHLa_UnC") %>%
   # filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30"))) %>%
   ggplot()+
-  geom_line(aes(x = date_sampled, y = result, linetype = REGsites), size = 1) +
+  # geom_line(aes(x = date_sampled, y = result, linetype = REGsites), size = 1) +
   geom_hline(yintercept = 6.6, linetype='longdash', color = 'gray18', size = 1.5)+
-  geom_point(aes(x = date_sampled, y = result, color = result >6.6, shape = REGsites), size = 3) +
-  theme_classic()
+  geom_point(aes(x = date_sampled, y = result, color = result > 6.6, shape = REGsites), size = 3) +
+  theme_classic()+
+  theme(legend.title = element_blank(),  # everything in theme is strictly aesthetics
+        legend.position = "bottom",
+        legend.text = element_text(size=12),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=13),
+        axis.ticks = element_line(color='black'),
+        plot.caption = element_text(size=6, face='italic'),
+        axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black'),
+        axis.text.y = element_text(size=12, color='black'),
+        axis.ticks.x = element_line(color='black'),
+        title = element_text(size = 13, face='bold'),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(color='gray95'))+
+  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y')+
+  labs(x = '', y = chla_y_title,
+       title = "River Sites")
+
 
 dat4 %>%
   filter(WBID == "Lake" & component_short == "CHLa_UnC") %>%
   # filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30"))) %>%
   ggplot()+
-  geom_line(aes(x = date_sampled, y = result, color = site, linetype = REGsites), size = 1) +
+  # geom_line(aes(x = date_sampled, y = result, color = site, linetype = REGsites), size = 1) +
   geom_hline(yintercept = 11, linetype='longdash', color = 'gray18', size = 1.5)+
-  geom_point(aes(x = date_sampled, y = result, color = result >11, shape = REGsites), size = 3) +
-  theme_classic()
+  geom_point(aes(x = date_sampled, y = result, color = result > 11, shape = REGsites), size = 3) +
+  theme_classic()+
+  theme(  # everything in theme is strictly aesthetics
+        legend.position = "bottom",
+        legend.text = element_text(size=12),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=13),
+        axis.ticks = element_line(color='black'),
+        plot.caption = element_text(size=6, face='italic'),
+        axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black'),
+        axis.text.y = element_text(size=12, color='black'),
+        axis.ticks.x = element_line(color='black'),
+        title = element_text(size = 13, face='bold'),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(color='gray95'))+
+  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y')+
+  labs(x = '', y = chla_y_title,
+       title = "Lake Sites")
 
 
 
