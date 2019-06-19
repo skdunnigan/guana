1ef537fe8b98d74b87d3c33c0e132ee5f05a0285
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-19 14:55:21 -0400

    skeletal framework for total nitrogen

--- R/02_totalnitrogen.R
+++ R/02_totalnitrogen.R
@@ -28,3 +28,58 @@ dat4 %>%
   scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b')+
   labs(x = '', y = 'Total Nitrogen (mg/L)',
        title = "Guana River")
+
+# all lake sites with open water and water control structure sites shown
+dat4 %>%
+  filter(WBID == "Lake" & component_short == "TN") %>%
+  # filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30"))) %>%
+  ggplot()+
+  geom_line(aes(x = date_sampled, y = result, color = site, linetype = sitetype), size = 1) +
+  # geom_hline(yintercept = 0.65, linetype='longdash', color = 'gray18', size = 1.5)+
+  geom_point(aes(x = date_sampled, y = result, color = site, shape = sitetype), size = 3) +
+  theme_classic()+
+  theme(legend.title = element_blank(),  # everything in theme is strictly aesthetics
+        legend.position = "bottom",
+        legend.text = element_text(size=12),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=13),
+        axis.ticks = element_line(color='black'),
+        plot.caption = element_text(size=6, face='italic'),
+        axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black'),
+        axis.text.y = element_text(size=12, color='black'),
+        axis.ticks.x = element_line(color='black'),
+        title = element_text(size = 13, face='bold'),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(color='gray95'))+
+  scale_y_continuous(expand = c(0,0))+
+  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y')+
+  labs(x = '', y = 'Total Nitrogen (mg/L)',
+       title = "Lake Sites",
+       caption = "Sites near water control structures are sampled for hydrologic connectivity and not used for waterbody assessments")
+
+# all river sites with regulation sites and excluded sites shown
+dat4 %>%
+  filter(WBID == "River" & component_short == "TN") %>%
+  # filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30"))) %>%
+  ggplot()+
+  geom_line(aes(x = date_sampled, y = result, color = site, linetype = sitetype), size = 1) +
+  geom_hline(yintercept = 0.65, linetype='longdash', color = 'gray18', size = 1.5)+
+  geom_point(aes(x = date_sampled, y = result, color = site, shape = sitetype), size = 3) +
+  theme_classic()+
+  theme(legend.title = element_blank(),  # everything in theme is strictly aesthetics
+        legend.position = "bottom",
+        legend.text = element_text(size=12),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=13),
+        axis.ticks = element_line(color='black'),
+        plot.caption = element_text(size=6, face='italic'),
+        axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black'),
+        axis.text.y = element_text(size=12, color='black'),
+        axis.ticks.x = element_line(color='black'),
+        title = element_text(size = 13, face='bold'),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(color='gray95'))+
+  scale_y_continuous(expand = c(0,0))+
+  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y')+
+  labs(x = '', y = 'Total Nitrogen (mg/L)',
+       title = "River Sites")
