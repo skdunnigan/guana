cae7176c6fb552668c6a3a9527d031353cd1aae2
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-21 15:12:21 -0400

    Reorganized and updated code

--- R/01_load_wrangle.R
+++ R/01_load_wrangle.R
@@ -1,9 +1,11 @@
 # code is to bring in data file and do some basic data tidying
 source('R/00_loadpackages.R')
 
-# read in guana data file
+# ---------------------------------------------------
+# read in guana data file\
+# ---------------------------------------------------
 dat <- read_xlsx("data/data-0619.xlsx", sheet = 'Sheet1') %>% janitor::clean_names()
-#janitor and clean_names() function cleans up the column header names!
+#janitor::clean_names() function cleans up the column header names!
 
 # inspect the data file
 head(dat)
@@ -13,11 +15,13 @@ str(dat)
 # data type housekeeping
 # ---------------------------------------------------
 # set 'result' column to numerical value, this column contains all the numerical values from the analyses
-dat$result <- as.numeric(dat$result)
-dat$mdl <- as.numeric(dat$mdl)
-dat$mrl <- as.numeric(dat$mrl)
+cols.num <- c("result", "mdl", "pql", "mrl", "dilution")
+dat[cols.num] <- sapply(dat[cols.num],as.numeric)
+sapply(dat, class)
+rm(cols.num)
 
-# convert date_received and date_analyzed into POSIXct format
+# convert datetimes into POSIXct format
+dat$date_sampled<- as.POSIXct(dat$date_sampled, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
 dat$date_received <- as.POSIXct(dat$date_received, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
 dat$date_analyzed <- as.POSIXct(dat$date_analyzed, format = "%m/%d/%Y %H:%M", tz = 'America/Regina')
 
@@ -25,3 +29,27 @@ dat$date_analyzed <- as.POSIXct(dat$date_analyzed, format = "%m/%d/%Y %H:%M", tz
 # should be the same number of both!
 unique(dat$component_short)
 unique(dat$component_long)
+
+# ---------------------------------------------------
+# read in tolomato met file
+# ---------------------------------------------------
+met_dat <- read_xlsx("data/tolomato.xlsx") %>% janitor::clean_names()
+
+# inspect the data file
+head(met_dat)
+str(met_dat)
+
+# ---------------------------------------------------
+# data type housekeeping
+# ---------------------------------------------------
+# set 'rainfall_mm' column to numerical value, this column contains all the numerical values from the analyses
+cols.num <- c("battery_v", "water_temp_c", "water_conductivity_m_s_cm",
+              "water_salinity_ppt", "air_temp_c", "relative_humidity_percent",
+              "barometric_pressure_in_hg", "rainfall_mm", "navd88_water_level_m")
+met_dat[cols.num] <- sapply(met_dat[cols.num],as.numeric)
+sapply(met_dat, class)
+
+# pull out date information
+met_dat$month <- month(met_dat$date_time_est)
+met_dat$day <- day(met_dat$date_time_est)
+met_dat$year <- as.character(year(met_dat$date_time_est))
