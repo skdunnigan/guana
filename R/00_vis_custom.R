846b7ab325a1a9e6ddd64b53026654480feef207
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-21 16:49:15 -0400

    Added visual palettes and labels for figures

---
+++ R/00_vis_custom.R
@@ -0,0 +1,18 @@
+# visual customizations for plots
+
+# create label for chlorophyll plots
+chla_y_title <- expression(paste("Chlorophyll ", italic("a "), mu*"g/L"))
+
+# create color palette for sites
+sitecolours <- c(
+  Micklers = "#F8766D",
+  `FDEP Lake 1` = "#D89000",
+  `FDEP Lake 2` = "#A3A500",
+  `Lake Middle` = "#39B600",
+  `FDEP Lake 4` = "#00BF7D",
+  `Lake South` = "#00BFC4",
+  `River North` = "#00B0F6",
+  `FDEP River 1` = "#9590FF",
+  `Guana River` = "#E76BF3",
+  `FDEP River 3` = "#FF62BC"
+)
