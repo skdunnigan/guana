cddb0664f63ca4d1ce903a3e0ef3df83c1acb4cd
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-24 16:53:03 -0400

    updated

--- R/06_guana_nut_wide.R
+++ R/06_guana_nut_wide.R
@@ -2,21 +2,22 @@
 source('R/01_load_wrangle.R')
 source('R/01_guana_wrangle_tidy.R')
 
-# this is to make data wider
+# this is to make data wider, good for qaqc
 
 dat_nut_alpha <- dat4 %>%
-  select(site, station_code, date_sampled, REGsites, WBID, component_short, result)
+  select(site, station_code, date_sampled, sitetype, WBID, component_short, result)
 
-dat_nut_beta <- dat_nut_alpha %>%
+dat_nut <- dat_nut_alpha %>%
   group_by(station_code, date_sampled, component_short) %>%
-  mutate(count = seq(n()), newcolumn = paste0(component_short, "_", count))
+  spread(key = component_short, value = result)
 
-dat_nut_gamma <- dat_nut_beta %>%
-  ungroup() %>%
-  select(-component_short, -count)
+rm(dat_nut_alpha)
 
-dat_nut <- dat_nut_gamma %>%
-  spread(key = newcolumn, value = result)
+# replace all NAs with blanks
+# this is a new dataframe because it will make everything factors
+# this is JUST to export the data into a csv without NAs
+dat_nut2 <- sapply(dat_nut, as.character)
+dat_nut2[is.na(dat_nut2)] <- " "
+dat_nut2<-as.data.frame(dat_nut2)
 
-rm(dat_nut_alpha, dat_nut_beta, dat_nut_gamma)
-write_csv(dat_nut, "dat_nut.csv")
+write_csv(dat_nut2, "output/data_wide.csv")
