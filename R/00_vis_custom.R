5ebd86649b4da43a5482c495c322db809ce8bbbd
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-24 10:40:02 -0400

    added other figure titles

--- R/00_vis_custom.R
+++ R/00_vis_custom.R
@@ -2,6 +2,15 @@
 
 # create label for chlorophyll plots
 chla_y_title <- expression(paste("Chlorophyll ", italic("a "), mu*"g/L"))
+# create label for phosphorus plots
+phos_y_title <- expression(paste("Total Phosphorus mg/L"))
+# create label for nitrogen plots
+nitro_y_title <- expression(paste("Total Nitrogen mg/L"))
+# create label for fecal plots
+fecal_y_title <- expression(paste("Fecal coliform CFU/100mL"))
+# create label for enterococcus plots
+entero_y_title <- expression(paste("Enterococcus MPN/100mL"))
+
 
 # create color palette for sites
 sitecolours <- c(
