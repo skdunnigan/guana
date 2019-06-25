78ea45302d92e10b6c50cfa47745f50dded5f8da
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-06-25 16:22:13 -0400

    updated packages used

--- R/00_loadpackages.R
+++ R/00_loadpackages.R
@@ -11,3 +11,9 @@ library(rmarkdown)
 library(janitor)
 library(here)
 library(SWMPr)
+library(reshape2)
+library(ggcorrplot)
+# or install the latest ggcorrplot from GitHub
+# Install
+# if(!require(devtools)) install.packages("devtools")
+# devtools::install_github("kassambara/ggcorrplot")
