d93186483987dfea59a7377fd2bbbd10fdd94f1a
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2019-07-25 11:51:54 -0400

    updated agms code

--- R/agms.R
+++ R/agms.R
@@ -2,19 +2,86 @@
 
 # filter between year 1 and year 2
 # filter by WBID with only the open water sites
-# calculate AGM for chla, TN, TP, Enterococcus, and DO.
+# then will filter by chla, TN, TP, Enterococcus, and DO.
 
-dat_y1 <- dat4 %>%
+# create functions for geometric means and standard deviations
+gmean <- function(x) exp(mean(log(x), na.rm = TRUE))
+gmeansd <- function(x) exp(sd(log(x), na.rm = TRUE))
+
+#### LAKE ####
+# agm tables for open water sites (used in assessment) in the lake for each year by
+# site Y1
+agms_l_site_year1 <- dat4 %>%
   filter(between(date_sampled, as.POSIXct("2017-07-01"), as.POSIXct("2018-06-30")) &
            WBID == "Lake" &
-           sitetype == "OpenWater")
+           sitetype == "OpenWater") %>%
+  group_by(site, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
 
-dat_y2 <- dat4 %>%
+# WBID Y1
+agms_l_WBID_year1 <- dat4 %>%
+  filter(between(date_sampled, as.POSIXct("2017-07-01"), as.POSIXct("2018-06-30")) &
+           WBID == "Lake" &
+           sitetype == "OpenWater") %>%
+  group_by(WBID, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
+
+# site Y2
+agms_l_site_year2 <- dat4 %>%
   filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30")) &
            WBID == "Lake" &
-           sitetype == "OpenWater")
+           sitetype == "OpenWater") %>%
+  group_by(site, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
 
-dat_y2 %>%
-  filter(component_short == "CHLa_C" & site == "Lake Middle") %>%
-  select(result) %>%
-  psych::geometric.mean(na.rm = TRUE)
+# WBID Y2
+agms_l_WBID_year2 <- dat4 %>%
+  filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30")) &
+           WBID == "Lake" &
+           sitetype == "OpenWater") %>%
+  group_by(WBID, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
+
+#### RIVER ####
+# agm tables for open water sites (used in assessment) in the river for each year by
+# site Y1
+agms_r_site_year1 <- dat4 %>%
+  filter(between(date_sampled, as.POSIXct("2017-07-01"), as.POSIXct("2018-06-30")) &
+           WBID == "River" &
+           sitetype == "OpenWater") %>%
+  group_by(site, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
+
+# WBID Y1
+agms_r_WBID_year1 <- dat4 %>%
+  filter(between(date_sampled, as.POSIXct("2017-07-01"), as.POSIXct("2018-06-30")) &
+           WBID == "River" &
+           sitetype == "OpenWater") %>%
+  group_by(WBID, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
+
+# site Y2
+agms_r_site_year2 <- dat4 %>%
+  filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30")) &
+           WBID == "River" &
+           sitetype == "OpenWater") %>%
+  group_by(site, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
+
+# WBID Y2
+agms_r_WBID_year2 <- dat4 %>%
+  filter(between(date_sampled, as.POSIXct("2018-07-01"), as.POSIXct("2019-06-30")) &
+           WBID == "River" &
+           sitetype == "OpenWater") %>%
+  group_by(WBID, component_short) %>%
+  summarise(means = gmean(result),
+            sd = gmeansd(result))
+
+# create tables for
