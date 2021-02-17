aa7f304ceb18d19399c1734f16d85da43da8da2b
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-02-17 17:45:50 -0500

    updated to add DSS data and also remove plotting fxns to new script

--- R/01.1_guana_load_wrangle_tidy.R
+++ R/01.1_guana_load_wrangle_tidy.R
@@ -1,22 +1,27 @@
 # code is to bring in data file and do some basic data tidying
 # if you have not run the 00_loadpackages.R you can uncomment the line below and run it
-source('R/00_loadpackages.R')
+# source('R/00_loadpackages.R')
 
 # if you have not run the  '00_vis_custom.R' you can uncomment the line below and run it
 # gives you specific axis titles and establishes specific colors for sites.
-source('R/00_vis_custom.R')
+# source('R/00_vis_custom.R')
 
 
-# ----01a LOAD read in data files -----------------------------------
+# ----01 LOAD read in data files -----------------------------------
 # janitor::clean_names() function cleans up the column header names!
 
 # guana nutrient data
-dat <- readxl::read_xlsx(here::here('data', 'guana_master_07.17-20.xlsx'),
+dat <- readxl::read_xlsx(here::here('data', 'guana_master_updated_2021.02.15.xlsx'),
                          sheet = 'Sheet1') %>%
   janitor::clean_names()
 
 # data dictionary with site-specific information
 dict <- readr::read_csv(here::here('data', 'guana_data_dictionary.csv')) %>%
+  janitor::clean_names()
+
+# prodss handheld data
+dss <- readxl::read_xlsx(here::here('data', 'Guana_Nut_Run_Master_2019_2020.xlsx'),
+                         sheet = 'Data') %>%
   janitor::clean_names()
 
 
@@ -25,7 +30,7 @@ head(dat)
 str(dat)
 dplyr::glimpse(dat) # this one is my favorite to use
 
-# ---- 01b CLEAN and MERGE: important/relevant columns -----------------------------------
+# ---- 02 CLEAN and MERGE: important/relevant columns -----------------------------------
 # then left_join() the dat data frame to the dict data frame to add additional columns
 dat2 <- dat %>%
   dplyr::filter(station_code != "GTMOLNUT_dup") %>%  # remove the 'duplicate' station that was only sampled for a short while
@@ -45,8 +50,22 @@ dat2 <- dat %>%
 # keeps 'dat' as ORIGINAL data that you read in
 dat2 <- dplyr::left_join(dat2, dict, by = "station_code")
 
+# and to merge the dss and dict dataframes
+dss2 <- dss %>%
+  dplyr::mutate(site = toupper(site)) %>%
+  dplyr::filter(!(site %in% c("D", "guana lake"))) %>%
+  dplyr::rename(site_acronym = site)
 
-# ---- 01c CHECK for spelling and duplication errors in data -----------------------------------
+# CAUTION: rewrites over dat2 dataframe created in previous lines
+# keeps 'dat' as ORIGINAL data that you read in
+    dict2 <- dict %>%
+      dplyr::filter(end == "N")
+
+dss2 <- dplyr::left_join(dss2, dict2, by = "site_acronym")
+
+    rm(dict2)
+
+# ---- 03 CHECK for spelling and duplication errors in data -----------------------------------
 
 ## This is where you can find entry errors. If you notice inconsistencies,
 ## you can correct them in excel, save, and run the previous lines of code again with a new file
@@ -65,7 +84,7 @@ View(dat2 %>%
   dplyr::filter(is.na(result))
 )
 
-# ----02a WRANGLE & TIDY -----------------------------------
+# ----04 WRANGLE & TIDY -----------------------------------
 
 # set 'result' column to numerical class
 # convert datetimes into POSIXct format
@@ -105,7 +124,7 @@ dat2 <- dat2 %>%
                                                   "GR3"))
                 )
 
-# ---- 02b CHECK your data again -----------------------------------
+# ---- 05 CHECK your data again -----------------------------------
 
 # check components again, to be sure
 unique(dat2$component_short)
@@ -123,221 +142,3 @@ janitor::get_dupes(dat2)
 View(dat2 %>%
        dplyr::filter(is.na(result))
 )
-
-# ----03 timeseries-all sites function -----------------------------------
-
-
-all_sites <- function(param, axis_title) {
-  # param - use component_short parameter name in quotes
-  # axis_title - use axis title value from 00_vis_custom.R, no quotes
-
-p <- dat2 %>%
-  dplyr::filter(component_short == param & end == "N") %>%
-  ggplot(aes(x = date_sampled, y = result, color = site_friendly)) +
-  geom_point(size = 3) +
-  geom_line(size = 1) +
-  scale_colour_manual(name = "Site", values = sitecolours) +
-  cowplot::theme_cowplot() +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y') +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black')) +
-  labs(y = axis_title,
-       x = "",
-       title = paste(param))
-
-p
-}
-
-# use the function to create full timeseries plots of whatever parameter you want, examples below
-
-all_sites("CHLA_C", chla_y_title) # corrected chlorophyll plot
-all_sites("TP", phos_y_title) # total phosphorus
-all_sites("TN", nitro_y_title) # total nitrogen (this may be incomplete because some values may end up needing to be calculated)
-all_sites("ENTERO", entero_y_title) # enterococcus
-all_sites("FECCOL", fecal_y_title) # fecal coliform
-
-# ---- 03a EXAMPLES of how to further customize -----------------------------------
-
-# not all parameters have a special y_axis title,
-# if you want to label axis that isn't a value from the 00_vis_custom.R,
-# you can do so using quotes. As an example:
-all_sites("SALT", "Salinity (psu)")
-
-# to change title name if you don't like default:
-all_sites("CHLA_C", chla_y_title) +
-  labs(title = "New title, you'll want to change me")
-
-# you can also change the scales of the y axis
-# (notice the difference between this output and the one from the previous line of code)
-all_sites("CHLA_C", chla_y_title) +
-  scale_y_continuous(breaks = c(25, 50, 75, 100, 125, 150, 200))
-
-# to do both:
-all_sites("CHLA_C", chla_y_title) +
-  scale_y_continuous(breaks = c(25, 50, 75, 100, 125, 150, 200))+
-  labs(title = "New title, you'll want to change me")
-
-# ---- 04 timeseries-split by waterbody function -----------------------------------
-# you are going to want to have lake and river split into two separate graphs
-
-waterbody_sites <- function(param, lake_threshold, river_threshold, axis_title) {
-  # param - use component_short parameter name in quotes
-  # axis_title - use axis title value from 00_vis_custom.R, no quotes
-  # lake_threshold - as number
-  # river_threshold - as number
-
-lake <- dat2 %>%
-  dplyr::filter(component_short == param & end == "N") %>%
-  dplyr::filter(wbid == "Lake") %>%
-  ggplot(aes(x = date_sampled, y = result, color = site_friendly)) +
-  geom_point(size = 3) +
-  geom_line(size = 1) +
-  geom_hline(yintercept = lake_threshold, linetype = 'longdash', color = 'gray18', size = 1.5) +
-  scale_colour_manual(name = "Site", values = sitecolours) +
-  cowplot::theme_cowplot() +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y') +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black')) +
-  labs(y = axis_title,
-       x = "",
-       title = "Lake",
-       subtitle = paste0("Threshold =", lake_threshold))
-
-river <- dat2 %>%
-  dplyr::filter(component_short == param & end == "N") %>%
-  dplyr::filter(wbid == "River") %>%
-  ggplot(aes(x = date_sampled, y = result, color = site_friendly)) +
-  geom_point(size = 3) +
-  geom_line(size = 1) +
-  geom_hline(yintercept = river_threshold, linetype = 'longdash', color = 'gray18', size = 1.5) +
-  scale_colour_manual(name = "Site", values = sitecolours) +
-  cowplot::theme_cowplot() +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_x_datetime(date_breaks = '1 month', date_minor_breaks = '2 weeks', date_labels='%b-%y') +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.3, size=12, color='black')) +
-  labs(y = axis_title,
-       x = "",
-       title = "River",
-       subtitle = paste0("Threshold =", lake_threshold))
-
-p <- cowplot::plot_grid(lake, river,
-                   ncol = 1)
-
-p
-
-}
-
-# ---- 04a EXAMPLES-----------------------------------
-waterbody_sites("CHLA_C", 11, 6.6, chla_y_title)
-
-waterbody_sites("CHLA_C", lake_threshold = "", 6.6, chla_y_title)
-
-# ---- 05 boxplots of all sites ----
-
-boxplot_all_sites <- function(param, axis_title) {
-  # param - use component_short parameter name in quotes
-  # axis_title - use axis title value from 00_vis_custom.R, no quotes
-
-  p <- dat2 %>%
-    dplyr::filter(component_short == param & end == "N") %>%
-    ggplot(aes(x = site_friendly, y = result, fill = site_friendly)) +
-    geom_boxplot(alpha = 0.8) +
-    scale_fill_manual(name = "Site", values = sitecolours) +
-    cowplot::theme_cowplot() +
-    scale_y_continuous(expand = c(0,0)) +
-    theme(axis.text.x = element_text(size=12, color='black'),
-          legend.position = "none") +
-    scale_x_discrete(labels = c("Micklers",
-                                "GL1",
-                                "GL2",
-                                "Lake\nMiddle",
-                                "GL4",
-                                "Lake\nSouth",
-                                "River\nNorth",
-                                "GR1",
-                                "Guana\nRiver",
-                                "GR3")) +
-    labs(y = axis_title,
-         x = "",
-         title = paste(param))
-
-  p
-}
-
-# use the function to create box plots of whatever parameter you want, examples below
-boxplot_all_sites("CHLA_C", chla_y_title)
-
-# this function works the same as `all_sites()` for changing titles
-boxplot_all_sites("CHLA_C", chla_y_title) +
-  labs(title = "New title, you'll want to change me")
-
-# or different parameters without designated title
-boxplot_all_sites("SALT", "Salinity (psu)")
-
-# ---- 06 boxplots by waterbody ----
-
-boxplot_wbid <- function(param, axis_title) {
-  # param - use component_short parameter name in quotes
-  # axis_title - use axis title value from 00_vis_custom.R, no quotes
-
-  p <- dat2 %>%
-    dplyr::filter(component_short == param & end == "N") %>%
-    dplyr::filter(wbid == "Lake") %>%
-    ggplot(aes(x = site_friendly, y = result, fill = site_friendly)) +
-    geom_boxplot(alpha = 0.8) +
-    scale_fill_manual(name = "Site", values = sitecolours) +
-    cowplot::theme_cowplot() +
-    scale_y_continuous(expand = c(0,0)) +
-    theme(axis.text.x = element_text(size=12, color='black'),
-          legend.position = "none") +
-    scale_x_discrete(labels = c("Micklers",
-                                "GL1",
-                                "GL2",
-                                "Lake\nMiddle",
-                                "GL4",
-                                "Lake\nSouth",
-                                "River\nNorth",
-                                "GR1",
-                                "Guana\nRiver",
-                                "GR3")) +
-    labs(y = axis_title,
-         x = "",
-         title = paste(param),
-         subtitle = "Guana Lake Sites")
-
-  q <- dat2 %>%
-    dplyr::filter(component_short == param & end == "N") %>%
-    dplyr::filter(wbid == "River") %>%
-    ggplot(aes(x = site_friendly, y = result, fill = site_friendly)) +
-    geom_boxplot(alpha = 0.8) +
-    scale_fill_manual(name = "Site", values = sitecolours) +
-    cowplot::theme_cowplot() +
-    scale_y_continuous(expand = c(0,0)) +
-    theme(axis.text.x = element_text(size=12, color='black'),
-          legend.position = "none") +
-    scale_x_discrete(labels = c("Micklers",
-                                "GL1",
-                                "GL2",
-                                "Lake\nMiddle",
-                                "GL4",
-                                "Lake\nSouth",
-                                "River\nNorth",
-                                "GR1",
-                                "Guana\nRiver",
-                                "GR3")) +
-    labs(y = axis_title,
-         x = "",
-         title = paste(param),
-         subtitle = "Guana River Sites")
-
-p / q
-
-}
-
-
-# use the function to create box plots of whatever parameter you want, examples below
-boxplot_wbid("CHLA_C", chla_y_title)
-
-
-# or different parameters without designated title
-boxplot_wbid("SALT", "Salinity (psu)")
